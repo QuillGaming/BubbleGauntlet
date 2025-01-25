1c684d9526cf82412d9008c84bf760b0a93bf95d
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-01-25 14:05:18 -0500

    Changes to scene handling
    Added audio to menus, got main menu, gametransition, and credits fully functional.

---
+++ Assets/Scripts/SceneManagement/GameTransition.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Linq;
+using TMPro;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTransition : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text Text;
+    [SerializeField]
+    private AudioSource pop;
+    private char game;
+
+    void Start()
+    {
+        string remainingGames = PlayerPrefs.GetString("RemainingGames");
+        if (remainingGames == "")
+        {
+            pop.Play();
+            Text.text = "Congratulations! You won " + PlayerPrefs.GetInt("Wins").ToString() + 
+                " out of 4 minigames!";
+            Invoke("GoToCredits", 3.0f);
+        }
+        else
+        {
+            pop.Play();
+            string gamesLeft = "";
+            int numGames = remainingGames.Length;
+            int i = Random.Range(0, numGames);
+            game = remainingGames[i];
+            string gameStr = game.ToString();
+
+            for (int j = 0; j < numGames; j++)
+            {
+                if (j.ToString() != gameStr)
+                {
+                    gamesLeft += remainingGames[j];
+                }
+            }
+
+            PlayerPrefs.SetString("remainingGames", gamesLeft);
+            PlayerPrefs.Save();
+            StartCoroutine("Transition");
+        }
+    }
+
+    private void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Text.text = "3";
+        pop.Play();
+        yield return new WaitForSeconds(1.0f);
+        Text.text = "2";
+        pop.Play();
+        yield return new WaitForSeconds(1.0f);
+        Text.text = "1";
+        pop.Play();
+        yield return new WaitForSeconds(1.0f);
+        Text.text = "Go!!!";
+        pop.Play();
+        yield return new WaitForSeconds(0.5f);
+        switch (game)
+        {
+            case '0':
+                SceneManager.LoadScene("BubbleBox");
+                break;
+            case '1':
+                SceneManager.LoadScene("BubbleWrap");
+                break;
+                /*
+            case "2":
+                SceneManager.LoadScene("Platform");
+            case "3":
+                SceneManager.LoadScene("");
+                break;
+                */
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagement/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource pop;
+    public void StartGame()
+    {
+        string games = "01";
+        int wins = 0;
+        PlayerPrefs.SetString("RemainingGames", games);
+        PlayerPrefs.SetInt("Wins", wins);
+        PlayerPrefs.Save();
+        pop.Play();
+        StartCoroutine(SceneLoad("GameTransition"));
+    }
+
+    public void Credits()
+    {
+        pop.Play();
+        StartCoroutine(SceneLoad("Credits"));
+    }
+
+    public void Exit()
+    {
+        pop.Play();
+        Application.Quit();
+    }
+
+    IEnumerator SceneLoad(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
