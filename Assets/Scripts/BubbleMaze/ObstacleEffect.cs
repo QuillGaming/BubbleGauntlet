a9931378fc1c3a361fa5173604e9cdb8970788f8
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-01-26 16:09:26 -0500

    Updated obstacleEffect
    ObstacleEffect now fails the player and sends them to the next level. Also made the bubble disappear and a pop sound play on collision.

--- Assets/Scripts/BubbleMaze/ObstacleEffect.cs
+++ Assets/Scripts/BubbleMaze/ObstacleEffect.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class ObstacleEffect : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject GameOver;
+
+    [SerializeField]
+    private AudioSource Pop;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("fail");
+            other.gameObject.SetActive(false);
+            Pop.Play();
+            GameOver.GetComponent<MinigameOver>().HandleGame(false);
         }
     }
 }
